53103e761c160ad2e4bf01293eab5e621f9468c4
antoRados <antorados@gmail.com>
2021-10-01 12:32:31 -0400

    Week 5 Assessment initial commit

---
+++ back-end/server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["There is good luck in your future!",
+					 "You shall do well on this assessment!",
+					 "DevMountain will help you land a great job!",
+           "The Texans will win on Sunday!",
+           "The Browns will lose this week",
+           "You stock investments will take off!",
+           "You will finally dunk a basketball!"
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortunes = fortunes[randomIndex];
+
+  res.status(200).send(randomFortunes);
+  
+});
+
+app.listen(4000, () => console.log("Server running on 4000"));
